0e0c4276790c8c7872ad510a3ce765244c84a9d7
najah <najahaboobacker79@gmail.com>
2023-09-11 23:13:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [todolist, setTodolist] = useState([]);
+  const [newtask,setNewtask] = useState("")
+  const handleChange=(event)=>{
+    setNewtask(event.target.value);
+  };
+    const addtask =()=>{
+      const newtodolist=[...todolist,newtask]
+      setTodolist(newtodolist);
+    };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addtask">
+        <input onChange={handleChange} />
+        <button onClick={addtask}>add task</button>
+      </div>
+      <div className="list">
+        {todolist.map((task )=> {
+          return <h1>{task}</h1>
+
+        })}
+      </div>
     </div>
   );
 }
